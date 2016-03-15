7253e9dbc8bedc247e84fad1c4f2c2b26bf92727
andrewgy8 <andrewgy8@gmail.com>
2016-03-15 17:53:58 +0800

    Grid runs on function

--- easscript.js
+++ easscript.js
@@ -1,25 +1,41 @@
-/*JavaScript variables*/
-var row = 16;
-var column = 16;
 
-/*JQuery variable*/
-var $row = $("<div />", {class: 'row'});
-var $box = $("<div />", {class: 'box'});
-var $solid = $("<div />", {class: 'solid'});
 
 /*JQuery and Javascript*/
 
 $(document).ready(function(){
-	for(var i = 0; i < row; i++){
-		$row.append($box.clone());
-	};
+	
 
-	for(var i = 0; i < column; i++){
-		$('#wrapper').append($row.clone());
-	};
+	/*JQuery variable*/
+	var $row = $("<div />", {class: 'row'});
+	var $box = $("<div />", {class: 'box'});
+	var $solid = $("<div />", {class: 'solid'});
+
+	
+
+	var gridCreate = function(){
+		var row = 16;
+		var column = 16;
+		for(var i = 0; i < row; i++){
+			$row.append($box.clone());
+		};
+
+		for(var i = 0; i < column; i++){
+			$('#wrapper').append($row.clone());
+		};
+	};	
+	gridCreate();
 
 	$('.box').mouseenter(function(){
 		$(this).addClass('solid');
+	});
+
+	/*Prompt*/
+
+	$('#clear').click(function(){
+		var row = prompt('How many rows would you like to have?');
+		var column = prompt('And how many columns would you like to have?');
+
+
 	});
 	
 });
@@ -27,6 +43,9 @@ $(document).ready(function(){
 
 
 /*	delete box
+
+
+	
 $('.box').mouseleave(function(){
 		$('.box').
 
