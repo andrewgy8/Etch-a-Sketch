2f416e67025b447244c46cab52e8e913f0f7ae02
andrewgy8 <andrewgy8@gmail.com>
2016-04-07 16:43:52 +0800

    Bug fix with clearing the grid, added the _row clone function

---
+++ Readme.txt
@@ -0,0 +1,13 @@
+Etch-a-Sketch
+
+A project by Andrew Graham-Yooll
+Project specifications provided by The Odin Project
+Written with JavaScript(jQuery Library), HTML, and CSS
+
+Started March 11, 2016
+	-initial grid functions
+	-clear button functionality 
+Updated April 6, 2016
+	-clear button bugs
+
+
--- easscript.js
+++ easscript.js
@@ -1,54 +1,54 @@
-
-
 /*JQuery and Javascript*/
 
 $(document).ready(function(){
-	
 
 	/*JQuery variable*/
+
 	var $row = $("<div />", {class: 'row'});
 	var $box = $("<div />", {class: 'box'});
 	var $solid = $("<div />", {class: 'solid'});
 
-	
+	/* Function for creating a grid given two attributes */
 
-	var gridCreate = function(){
-		var row = 16;
-		var column = 16;
-		for(var i = 0; i < row; i++){
-			$row.append($box.clone());
+	var gridCreate = function(input1, input2){
+		var rows 	= input1;
+		var columns = input2;
+		
+		/*Need to clone $row before $box.clone */
+		var _row 	= $row.clone();
+
+		for(i = 0; i < rows; i++){
+			_row.append($box.clone());
 		};
 
-		for(var i = 0; i < column; i++){
-			$('#wrapper').append($row.clone());
+		for(i = 0; i < columns; i++){
+			$('#wrapper').append(_row.clone());
 		};
+
+		$('.box').mouseenter(function(){
+			$(this).addClass('solid');
+		});
 	};	
-	gridCreate();
 
-	$('.box').mouseenter(function(){
-		$(this).addClass('solid');
+	/* Initial grid with 16x16 */
+	gridCreate(16, 16);
+
+	/*Clear function*/
+	var clearGrid = function () {
+		$("#wrapper").empty();
+	};	
+
+	/* The new grid function, propogated by a prompt */
+	var newGrid = function(){
+		var input1 = prompt('How many rows would you like to have (2-20)?');
+		var input2 = prompt('How many columns would you like?');
+		gridCreate(input1, input2);
+	}
+		
+	/***********************Event handler*************************/
+
+	$('#clear').on('click', function(){
+		clearGrid();
+		newGrid();
 	});
-
-	/*Prompt*/
-
-	$('#clear').click(function(){
-		var row = prompt('How many rows would you like to have?');
-		var column = prompt('And how many columns would you like to have?');
-
-
-	});
-	
 });
-
-
-
-/*	delete box
-
-
-	
-$('.box').mouseleave(function(){
-		$('.box').
-
-$('.box').append($solid);
-
-*/
